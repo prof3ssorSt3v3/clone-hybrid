4635ef3042f53f416c3b349b75879030d26eacf8
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2021-08-12 17:13:05 -0400

    Add initial files

---
+++ data.js
@@ -0,0 +1,22 @@
+/**
+ * Hybrid 7 JS file
+ * Name: Your name here
+ */
+
+const data = {
+  name: 'Your name here',
+  characters: [
+    { id: 1, name: 'Frodo', race: 'Hobbit' },
+    { id: 3, name: 'Gandalf', race: 'Wizard' },
+    { id: 5, name: 'Gimli', race: 'Dwarf' },
+    { id: 16, name: 'Legolas', race: 'Elf' },
+    { id: 19, name: 'Boromir', race: 'Human' },
+    { id: 22, name: 'TreeBeard', race: 'Ent' },
+    { id: 31, name: 'Gollum', race: 'Stoor Hobbit' },
+    { id: 44, name: 'Azog', race: 'Orc' },
+    { id: 55, name: 'Beorn', race: 'Shapeshifter' },
+    { id: 68, name: 'Shelob', race: 'Spider' },
+  ],
+};
+
+module.exports = data;
